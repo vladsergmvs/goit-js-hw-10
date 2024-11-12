56515925cf2f7139711d84880f247826f69a624c
vladsergmvs <72503462+vladsergmvs@users.noreply.github.com>
2024-11-12 17:41:33 +0000

    Deploying to gh-pages from @ vladsergmvs/goit-js-hw-10@4d07b834cfc8b05273a987d960b0cc83336d9f31 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";document.querySelector("input");const t=document.querySelector(".form");document.querySelector("fieldset");t.addEventListener("submit",e=>{e.preventDefault(),console.log()});
+//# sourceMappingURL=2-snackbar.js.map
